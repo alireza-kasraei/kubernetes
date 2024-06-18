9f51ff0db8731db4df58fe90c437251a9fc26423
ali <kasraei.ar@gmail.com>
2024-06-18 09:34:03 +0200

    update my rke

--- rancher/rke/bootstrap.sh
+++ rancher/rke/bootstrap.sh
@@ -8,4 +8,25 @@ systemctl reload sshd
 
 # Set Root password
 echo "[TASK 2] Set root password"
-echo -e "kubeadmin\nkubeadmin" | passwd root >/dev/null 2>&1
+echo -e "root\nroot" | passwd root >/dev/null 2>&1
+
+echo "[TASK 3] Set kernel parameters"
+cat >>/etc/sysctl.d/kubernetes.conf<<EOF
+net.bridge.bridge-nf-call-ip6tables = 1
+net.bridge.bridge-nf-call-iptables = 1
+vm.max_map_count=262144
+EOF
+sysctl --system
+
+echo "[TASK 4] disabling the firewall"
+ufw disable
+
+echo "[TASK 5] turning the swap off"
+swapoff -a; sed -i '/swap/d' /etc/fstab
+
+
+echo "[TASK 6] installing docker"
+apt install -y apt-transport-https ca-certificates curl gnupg-agent software-properties-common
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
+add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+apt update && apt install -y docker-ce=5:19.03.10~3-0~ubuntu-focal containerd.io
